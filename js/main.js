a4447c3dd8a56cd8d904659c5839da800a510166
calebbenjin <calebben94@gmail.com>
2021-01-24 09:08:48 +0100

    testing

--- js/main.js
+++ js/main.js
@@ -62,3 +62,17 @@ console.log(formContent)
   const activeCotent = document.querySelector(`.forms__content--${cliks.dataset.tab}`).classList.add('form__content--active')
   console.log(activeCotent)
 })
+
+function showPreviewOne(event){
+  if(event.target.files.length > 0){
+    let src = URL.createObjectURL(event.target.files[0]);
+    let preview = document.getElementById("file-ip-1-preview");
+    preview.src = src;
+    preview.style.display = "block";
+    document.querySelector('.cover-text').style.display = 'none'
+  } 
+}
+
+function myImgRemoveFunctionOne() {
+  document.getElementById("file-ip-1-preview").src = "";
+}
